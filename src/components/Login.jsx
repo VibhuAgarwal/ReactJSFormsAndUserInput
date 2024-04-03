c792d2374ef0ee370081dd05f3de20aa3d21c097
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-04-03 23:44:19 +0530

    Extracting values using useState by making object of values.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,56 @@
+import { useState } from "react";
+
 export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredValue, setEnteredValue] = useState({
+    email: '',
+    password: ''
+  });
+
+  function handleInputChange(identifier, value) {
+    setEnteredValue(prevValues => ({
+      ...prevValues,
+      [identifier]: value
+    }))
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(enteredValue);
+  }
+
+  // function handleEmailChange(e) {
+  //   setEnteredEmail(e.target.value);
+  // }
+
+  // function handlePasswordChange(e) {
+  //   setEnteredPassword(e.target.value);
+  // }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(e) => handleInputChange('email', e.target.value)}
+            value={enteredValue.email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(e) => handleInputChange('password', e.target.value)}
+            value={enteredValue.password} />
         </div>
       </div>
 
@@ -19,6 +58,6 @@ export default function Login() {
         <button className="button button-flat">Reset</button>
         <button className="button">Login</button>
       </p>
-    </form>
+    </form >
   );
 }
